183e03ef8fde385478a3e9ff524b9dea62d766d8
Nobutaka Ito <nobutaka-ito@platform-one.co.jp>
2016-05-11 20:21:39 +0900

    correct but too many steps

--- min-conflict.cc
+++ min-conflict.cc
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+#include <set>
 #include <unordered_set>
 #include <unordered_map>
 #include <queue>
@@ -17,49 +18,65 @@ class nqueen{
 public:
         nqueen(int n){
                 random_device rnd;
+                mt19937 mt(rnd());
                 size = n;
                 board = new int[n];
+                // for(int i=0;i<n;i++){
+                //         board[i] = -1;
+                // }
+                // initialize();
                 for(int i=0;i<n;i++){
-                        board[i] = rnd()%n;
-                }
-        }
-        nqueen(nqueen& before){
-                size = before.size;
-                board = new int[size];
-                for(int i=0;i<size;i++){
-                        board[i] = before.board[i];
+                        board[i] = mt()%n;
                 }
         }
         ~nqueen(){
                 delete board;
         }
-        int size;
-        int* board;
-        vector<int> conflictlist;
-        bool isSolution(){
-                conflictlist.clear();
-                unordered_set<int> index;
+        void firstconflict(){
                 for(int i=0;i<size;i++){
-                        if(index.find(i) != index.end())
+                        if(conflictindex.find(i) != conflictindex.end())
                                 continue;
-                        for(int j=i+1;j<size;j++){
+                        for(int j=0;j<size;j++){
                                 int diff = j-i;
                                 if(board[i]==board[j] || board[i]+diff == board[j] || board[i]-diff == board[j]){
-                                        if(index.find(i) == index.end()){
-                                                conflictlist.push_back(i);
-                                                index.insert(i);
+                                        if(conflictindex.find(i) == conflictindex.end()){
+                                                conflictindex.insert(i);
                                         }
-                                        if(index.find(j) == index.end()){
-                                                conflictlist.push_back(j);
-                                                index.insert(j);
+                                        if(conflictindex.find(j) == conflictindex.end()){
+                                                conflictindex.insert(j);
                                         }
                                 }
                         }
                 }
-                if(conflictlist.empty())
+        }
+        // void initialize(){
+        //         for(int i=0;i<size;i++){
+
+        //         }
+        // }
+        int size;
+        int* board;
+        unordered_set<int> conflictindex;
+        unordered_set<int> conflictcandidate;
+        bool isSolution(){
+                if(conflictindex.empty() && conflictcandidate.empty())
                         return true;
-                else
-                        return false;
+                for(auto it = conflictcandidate.begin();it!=conflictcandidate.end();it++){
+                        for(int i=0;i<size;i++){
+                                if(*it==i)
+                                        continue;
+                                int diff = abs(i-*it);
+                                if(board[*it]==board[i] || board[*it]+diff == board[i] || board[*it]-diff == board[i]){
+                                        if(conflictindex.find(*it) == conflictindex.end()){
+                                                conflictindex.insert(*it);
+                                        }
+                                }
+                        }
+                }
+                conflictcandidate.clear();
+                if(conflictindex.empty())
+                        return true;
+                return false;
         }
         void printBoard(){
                 for(int i=0;i<size;i++){
@@ -71,7 +88,7 @@ public:
                         }
                         cout << endl;
                 }
-                cout << endl;
+                // cout << endl;
         }
 };
 
@@ -84,39 +101,87 @@ int conflict(nqueen& state,int var){
         for(int i=0;i<state.size;i++){
                 if(var == i)
                         continue;
-                else{
-                        num = bh.find(state.board[i]);
+                num = bh.find(state.board[i]);
+                bh.a[num].second++;
+                bh.trickleDown(num);
+                diff = abs(var-i);
+                if(state.board[i]+diff < state.size){
+                        num = bh.find(state.board[i]+diff);
                         bh.a[num].second++;
                         bh.trickleDown(num);
-                        diff = abs(var-i);
-                        if(state.board[i]+diff < state.size){
-                                num = bh.find(state.board[i]+diff);
-                                bh.a[num].second++;
-                                bh.trickleDown(num);
-                        }
-                        if(0<= state.board[i]-diff){
-                                num = bh.find(state.board[i]-diff);
-                                bh.a[num].second++;
-                                bh.trickleDown(num);
+                }
+                if(0<= state.board[i]-diff){
+                        num = bh.find(state.board[i]-diff);
+                        bh.a[num].second++;
+                        bh.trickleDown(num);
+                }
+        }
+        vector<pair<int,int> > mins;
+        pair<int,int> min = bh.findMin();
+        bh.remove();
+        mins.push_back(min);
+        while(bh.findMin().second == mins[0].second){
+                mins.push_back(bh.findMin());
+                bh.remove();
+        }
+        random_device rnd;
+        int x = rnd()%mins.size();
+        return mins[x].first;
+}
+
+void eraseconflict(nqueen& state,int var, int value){
+        state.conflictindex.erase(var);
+        // state.conflictcandidate.insert(var);
+        for(int i=0;i<state.size;i++){
+                if(var==i)
+                        continue;
+                int diff = i-var;
+                if(value==state.board[i] || value+diff == state.board[i] || value-diff == state.board[i]){
+                        state.conflictindex.erase(i);
+                        state.conflictcandidate.insert(i);
+                }
+        }
+        return;
+}
+
+void addconflict(nqueen& state, int var,int value){
+        for(int i=0;i<state.size;i++){
+                if(var==i)
+                        continue;
+                int diff = i-var;
+                if(value==state.board[i] || value+diff == state.board[i] || value-diff == state.board[i]){
+                        if(state.conflictindex.find(i) == state.conflictindex.end()){
+                                state.conflictcandidate.insert(i);
+                                state.conflictcandidate.insert(var);
                         }
                 }
         }
-        return bh.findMin().first;
+        return;
 }
 
 bool minconflict(nqueen& state,int maxsteps){
         for(int i=0;i<maxsteps;i++){
-                // state.printBoard();
+                cout << i <<endl;
+                state.printBoard();
                 if(state.isSolution()){
                         // state.printBoard();
                         return true;
                 }
+                cout << " " << state.conflictindex.size() << endl <<endl;
                 random_device rnd;
                 //conflictを起こしてるやつをrandom抽出
-                int var = state.conflictlist[rnd()%state.conflictlist.size()];
+                // int var = state.conflictlist[rnd()%state.conflictlist.size()];
+                int x = rnd()%state.conflictindex.size();
+                auto it = state.conflictindex.begin();
+                for(int i=0;i<x;i++){
+                        it++;
+                }
+                int var = *it;
                 //conflictが一番少なくなる位置へ移動
                 int value = conflict(state,var);
+                eraseconflict(state,var,state.board[var]);
                 state.board[var] = value;
+                addconflict(state,var,value);
         }
         return false;
 }
@@ -124,10 +189,11 @@ bool minconflict(nqueen& state,int maxsteps){
 int main(){
         int n;
         cin >> n;
-        nqueen init(n);
         struct timeval start,goal;
+        nqueen init(n);
         gettimeofday(&start,NULL);
-        minconflict(init,10);
+        init.firstconflict();
+        minconflict(init,1000);
         gettimeofday(&goal,NULL);
         printTime(start,goal);
 }
